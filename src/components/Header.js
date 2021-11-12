97d08d1c0007a8c470af30eafa79a5118b433a65
vanissimo <eng.vano@yandex.ru>
2021-11-12 16:50:57 +0300

    add v 1.0

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Header = () => {
+    return ( <div className = "header">
+        <h1> To do list </h1>
+
+        </div>     
+    )
+}
+
+export default Header;
